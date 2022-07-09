e0ede0707159b6394d4b9b83c4b6128779a909f7
Neno <nebojsa.kovacevik@gmail.com>
2022-07-09 21:38:59 +0200

    Add layout for calculator

---
+++ script.js
@@ -0,0 +1,11 @@
+const add = (a, b) => a + b;
+
+const sub = (a, b) => a - b;
+
+const mult = (a, b) => a * b;
+
+const div = (a, b) => a / b;
+
+const operate = (op, a, b) => {
+  return op(a, b);
+};
